13ebdeb5f99b62bfb31a5a274775a3a11e8030ae
Pavel Gulchuk <gul@gul.kiev.ua>
2017-07-14 18:02:38 +0300

    Fixed warning

--- config.c
+++ config.c
@@ -514,7 +514,8 @@ static int parse_file(FILE *f, char *fname)
         }
         *p1='\0';
       } else
-      { for (p1=p; *p1 && !isspace(*p1); p1++);
+      {
+        for (p1=p; *p1 && !isspace(*p1); p1++);
         *p1='\0';
       }
       if ((finc=fopen(p, "r")) == NULL)
